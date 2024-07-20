f5f527e7ecb9d285e590d834712a72c86478cede
htkhiem <42716602+htkhiem@users.noreply.github.com>
2024-07-20 09:02:35 +0700

    Temporarily disable the marquee due to scrolling issues

--- src/player/queue_row.rs
+++ src/player/queue_row.rs
@@ -36,8 +36,8 @@ mod imp {
     pub struct QueueRow {
         #[template_child]
         pub thumbnail: TemplateChild<Image>,
-        #[template_child]
-        pub marquee: TemplateChild<Viewport>,
+        // #[template_child]
+        // pub marquee: TemplateChild<Viewport>,
         #[template_child]
         pub song_name: TemplateChild<Label>,
          #[template_child]
@@ -48,11 +48,11 @@ mod imp {
         pub playing_indicator: TemplateChild<Label>,
         // Vector holding the bindings to properties of the Song GObject
         pub bindings: RefCell<Vec<Binding>>,
-        pub playing_signal_id: RefCell<Option<SignalHandlerId>>,
+        // pub playing_signal_id: RefCell<Option<SignalHandlerId>>,
         pub thumbnail_signal_id: RefCell<Option<SignalHandlerId>>,
-        pub marquee_tick_callback_id: RefCell<Option<TickCallbackId>>,
-        pub marquee_forward: Cell<bool>,
-        pub marquee_progress: Cell<f64>
+        // pub marquee_tick_callback_id: RefCell<Option<TickCallbackId>>,
+        // pub marquee_forward: Cell<bool>,
+        // pub marquee_progress: Cell<f64>
     }
 
     // The central trait for subclassing a GObject
@@ -120,67 +120,67 @@ impl QueueRow {
         res
     }
 
-    fn start_marquee(&self) {
-        let marquee = self.imp().marquee.get();
-        let adj = marquee.hadjustment().expect("No adjustment?");
-        self.imp().marquee_forward.replace(true);
-        self.imp().marquee_progress.replace(0.0);
-        let this = self.clone();
-        let id = marquee.add_tick_callback(move |_, frame_clock| {
-            // TODO: customisable interval. For now hardcoding to 5000ms each direction (10s full cycle).
-            // Full range = upper - page_size, where page is the "content width" and upper is
-            // the maximum "coordinate" that can be seen by the ScrolledWindow, i.e. the far end
-            // of the content.
-            // Value on the other hand is the "coordinate" of the beginning of the content.
-            // Recalculate range at every tick since user might have resized the window.
-            let range = adj.upper() - adj.page_size();
-            if range > 0.0 {
-                let progress_step = (1000.0 / frame_clock.fps()) / 5000.0;  // in milliseconds
-                // Calculate progress value at next frame.
-                if this.imp().marquee_forward.get() {
-                    let next_progress = this.imp().marquee_progress.get() + progress_step;
-                    if next_progress >= 1.0 {
-                        // Do not advance. Instead, simply flip direction for next frame.
-                        let _ = this.imp().marquee_forward.replace(false);
-                    }
-                    else {
-                        // Not at the end yet => advance
-                        let next_value = ease_in_out_sine(next_progress);
-                        let _ = this.imp().marquee_progress.replace(next_progress);
-                        adj.set_value(next_value * range);
-                    }
-                }
-                else {
-                    let next_progress = this.imp().marquee_progress.get() - progress_step;
-                    if next_progress <= 0.0 {
-                        let _ = this.imp().marquee_forward.replace(true);
-                    }
-                    else {
-                        // Not at the end yet => advance
-                        let next_value = ease_in_out_sine(next_progress);
-                        let _ = this.imp().marquee_progress.replace(next_progress);
-                        adj.set_value(next_value * range);
-                    }
-                }
-            }
-            ControlFlow::Continue
-        });
-        if let Some(old_id) = self.imp().marquee_tick_callback_id.replace(Some(id)) {
-            old_id.remove();
-        }
-    }
+    // fn start_marquee(&self) {
+    //     let marquee = self.imp().marquee.get();
+    //     let adj = marquee.hadjustment().expect("No adjustment?");
+    //     self.imp().marquee_forward.replace(true);
+    //     self.imp().marquee_progress.replace(0.0);
+    //     let this = self.clone();
+    //     let id = marquee.add_tick_callback(move |_, frame_clock| {
+    //         // TODO: customisable interval. For now hardcoding to 5000ms each direction (10s full cycle).
+    //         // Full range = upper - page_size, where page is the "content width" and upper is
+    //         // the maximum "coordinate" that can be seen by the ScrolledWindow, i.e. the far end
+    //         // of the content.
+    //         // Value on the other hand is the "coordinate" of the beginning of the content.
+    //         // Recalculate range at every tick since user might have resized the window.
+    //         let range = adj.upper() - adj.page_size();
+    //         if range > 0.0 {
+    //             let progress_step = (1000.0 / frame_clock.fps()) / 5000.0;  // in milliseconds
+    //             // Calculate progress value at next frame.
+    //             if this.imp().marquee_forward.get() {
+    //                 let next_progress = this.imp().marquee_progress.get() + progress_step;
+    //                 if next_progress >= 1.0 {
+    //                     // Do not advance. Instead, simply flip direction for next frame.
+    //                     let _ = this.imp().marquee_forward.replace(false);
+    //                 }
+    //                 else {
+    //                     // Not at the end yet => advance
+    //                     let next_value = ease_in_out_sine(next_progress);
+    //                     let _ = this.imp().marquee_progress.replace(next_progress);
+    //                     adj.set_value(next_value * range);
+    //                 }
+    //             }
+    //             else {
+    //                 let next_progress = this.imp().marquee_progress.get() - progress_step;
+    //                 if next_progress <= 0.0 {
+    //                     let _ = this.imp().marquee_forward.replace(true);
+    //                 }
+    //                 else {
+    //                     // Not at the end yet => advance
+    //                     let next_value = ease_in_out_sine(next_progress);
+    //                     let _ = this.imp().marquee_progress.replace(next_progress);
+    //                     adj.set_value(next_value * range);
+    //                 }
+    //             }
+    //         }
+    //         ControlFlow::Continue
+    //     });
+    //     if let Some(old_id) = self.imp().marquee_tick_callback_id.replace(Some(id)) {
+    //         old_id.remove();
+    //     }
+    // }
 
-    fn stop_marquee(&self) {
-        let marquee = self.imp().marquee.get();
-        // Remove the marquee movement callback & set its position back to 0.
-        if let Some(id) = self.imp().marquee_tick_callback_id.take() {
-            id.remove();
-        }
-        let adj = marquee.hadjustment().expect("No adjustment?");
-        adj.set_value(
-            adj.lower()
-        );
-    }
+    // fn stop_marquee(&self) {
+    //     let marquee = self.imp().marquee.get();
+    //     // Remove the marquee movement callback & set its position back to 0.
+    //     if let Some(id) = self.imp().marquee_tick_callback_id.take() {
+    //         id.remove();
+    //     }
+    //     let adj = marquee.hadjustment().expect("No adjustment?");
+    //     adj.set_value(
+    //         adj.lower()
+    //     );
+    // }
 
     pub fn bind(&self, song: &Song) {
         // Get state
@@ -227,30 +227,30 @@ impl QueueRow {
         // Save binding
         bindings.push(artist_name_binding);
 
-        // let song_is_playing_binding = song
-        //     .bind_property("is-playing", &playing_label, "visible")
-        //     .sync_create()
-        //     .build();
-        // // Save binding
-        // bindings.push(song_is_playing_binding);
+        let song_is_playing_binding = song
+            .bind_property("is-playing", &playing_label, "visible")
+            .sync_create()
+            .build();
+        // Save binding
+        bindings.push(song_is_playing_binding);
 
         // Set once first (like sync_create)
-        if song.is_playing() {
-            self.start_marquee();
-        }
-        let playing_binding = song
-            .connect_notify_local(
-                Some("is-playing"),
-                clone!(@weak self as this => move |this_song, _| {
-                    if this_song.is_playing() {
-                        this.start_marquee();
-                    }
-                    else {
-                        this.stop_marquee();
-                    }
-                }),
-            );
-        self.imp().playing_signal_id.replace(Some(playing_binding));
+        // if song.is_playing() {
+        //     self.start_marquee();
+        // }
+        // let playing_binding = song
+        //     .connect_notify_local(
+        //         Some("is-playing"),
+        //         clone!(@weak self as this => move |this_song, _| {
+        //             if this_song.is_playing() {
+        //                 this.start_marquee();
+        //             }
+        //             else {
+        //                 this.stop_marquee();
+        //             }
+        //         }),
+        //     );
+        // self.imp().playing_signal_id.replace(Some(playing_binding));
     }
 
     pub fn unbind(&self, song: &Song) {
@@ -262,8 +262,8 @@ impl QueueRow {
             song.disconnect(id);
         }
 
-        if let Some(id) = self.imp().playing_signal_id.take() {
-            song.disconnect(id);
-        }
+        // if let Some(id) = self.imp().playing_signal_id.take() {
+        //     song.disconnect(id);
+        // }
     }
 }
