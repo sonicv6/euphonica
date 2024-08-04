5ccf28f97f6f921ceae2a5b398dcdb5373ad0045
htkhiem <42716602+htkhiem@users.noreply.github.com>
2024-08-04 11:45:08 +0700

    Remove old code

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,8 +1,6 @@
-pub mod albumart;
 pub mod wrapper;
 pub mod state;
 
 pub use state::{ClientState, ConnectionState};
 pub use wrapper::MpdWrapper;
 pub use wrapper::MpdMessage;
-pub use albumart::{AlbumArtCache, ALBUMART_PLACEHOLDER};
